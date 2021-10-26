9d7711d18fea3122c3f86c0f90cf632babde8a92
unknown <rsc16@students.waikato.ac.nz>
2021-10-26 13:25:14 +1300

    Added Caesar Cypher

---
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonEncrypt = new System.Windows.Forms.Button();
+            this.buttonDecrypt = new System.Windows.Forms.Button();
+            this.textBoxText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxFunctions = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxKey = new System.Windows.Forms.TextBox();
+            this.buttonFindKey = new System.Windows.Forms.Button();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cryptography Project";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // buttonEncrypt
+            // 
+            this.buttonEncrypt.Location = new System.Drawing.Point(12, 415);
+            this.buttonEncrypt.Name = "buttonEncrypt";
+            this.buttonEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.buttonEncrypt.TabIndex = 1;
+            this.buttonEncrypt.Text = "Encrypt";
+            this.buttonEncrypt.UseVisualStyleBackColor = true;
+            this.buttonEncrypt.Click += new System.EventHandler(this.buttonEncrypt_Click);
+            // 
+            // buttonDecrypt
+            // 
+            this.buttonDecrypt.Location = new System.Drawing.Point(93, 415);
+            this.buttonDecrypt.Name = "buttonDecrypt";
+            this.buttonDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.buttonDecrypt.TabIndex = 2;
+            this.buttonDecrypt.Text = "Decrypt";
+            this.buttonDecrypt.UseVisualStyleBackColor = true;
+            this.buttonDecrypt.Click += new System.EventHandler(this.buttonDecrypt_Click);
+            // 
+            // textBoxText
+            // 
+            this.textBoxText.Location = new System.Drawing.Point(12, 31);
+            this.textBoxText.Multiline = true;
+            this.textBoxText.Name = "textBoxText";
+            this.textBoxText.Size = new System.Drawing.Size(266, 141);
+            this.textBoxText.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Function";
+            // 
+            // comboBoxFunctions
+            // 
+            this.comboBoxFunctions.FormattingEnabled = true;
+            this.comboBoxFunctions.Items.AddRange(new object[] {
+            "Caesar Cypher"});
+            this.comboBoxFunctions.Location = new System.Drawing.Point(13, 193);
+            this.comboBoxFunctions.Name = "comboBoxFunctions";
+            this.comboBoxFunctions.Size = new System.Drawing.Size(121, 23);
+            this.comboBoxFunctions.TabIndex = 5;
+            this.comboBoxFunctions.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 219);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Key";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBoxKey
+            // 
+            this.textBoxKey.Location = new System.Drawing.Point(13, 237);
+            this.textBoxKey.Name = "textBoxKey";
+            this.textBoxKey.Size = new System.Drawing.Size(100, 23);
+            this.textBoxKey.TabIndex = 7;
+            // 
+            // buttonFindKey
+            // 
+            this.buttonFindKey.Location = new System.Drawing.Point(174, 415);
+            this.buttonFindKey.Name = "buttonFindKey";
+            this.buttonFindKey.Size = new System.Drawing.Size(75, 23);
+            this.buttonFindKey.TabIndex = 8;
+            this.buttonFindKey.Text = "Find Keys";
+            this.buttonFindKey.UseVisualStyleBackColor = true;
+            this.buttonFindKey.Click += new System.EventHandler(this.buttonFindKey_Click);
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Location = new System.Drawing.Point(12, 266);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.ReadOnly = true;
+            this.textBoxLog.Size = new System.Drawing.Size(266, 143);
+            this.textBoxLog.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(294, 450);
+            this.Controls.Add(this.textBoxLog);
+            this.Controls.Add(this.buttonFindKey);
+            this.Controls.Add(this.textBoxKey);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBoxFunctions);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxText);
+            this.Controls.Add(this.buttonDecrypt);
+            this.Controls.Add(this.buttonEncrypt);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonEncrypt;
+        private System.Windows.Forms.Button buttonDecrypt;
+        private System.Windows.Forms.TextBox textBoxText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxFunctions;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxKey;
+        private System.Windows.Forms.Button buttonFindKey;
+        private System.Windows.Forms.TextBox textBoxLog;
+    }
+}
+
---
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        //Encrypts the text
+        private void buttonEncrypt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string plainText = textBoxText.Text; //Get the plain text from the textbox
+                string key = textBoxKey.Text; //Get the key from the key textbox
+                string function = comboBoxFunctions.Text; //Get the text from the functions combo box
+                string cypherText = ""; //The cyphertext being returned
+                if (function == "Caesar Cypher")
+                {
+                    int caesarKey = int.Parse(key);
+                    cypherText = CaesarCypher(plainText, caesarKey, true);
+                }
+                textBoxText.Text = cypherText;
+            }
+            catch
+            {
+                MessageBox.Show("Validate your inputs");
+            }
+        }
+        //Decrypts the text
+        private void buttonDecrypt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string cypherText = textBoxText.Text; //Get the plain text from the textbox
+                string key = textBoxKey.Text; //Get the key from the key textbox
+                string function = comboBoxFunctions.Text; //Get the text from the functions combo box
+                string plainText = ""; //The cyphertext being returned
+                if (function == "Caesar Cypher")
+                {
+                    int caesarKey = int.Parse(key);
+                    plainText = CaesarCypher(cypherText, caesarKey, false);
+                }
+                textBoxText.Text = plainText;
+            }
+            catch
+            {
+                MessageBox.Show("Validate your inputs");
+            }
+        }
+        //Casear text
+        private string CaesarCypher(string text, int key, bool isEncryption)
+        {
+            StringBuilder caesarText = new StringBuilder();
+            key = key % 26; //Modulo 26
+            if (isEncryption)
+            {
+                //Go through each character of the plain text
+                for (int i = 0; i < text.Length; i++)
+                {
+                    char letter = text[i];
+                    int number = (int)letter; //Select the ASCII number of the capital letter
+                    //If they are capital
+                    if (letter >= 65 && letter <= 90)
+                    {
+                        number -= 65; //Subtract by 65
+                        number += key; //Add the key
+                        number %= 26; //Modulo 26
+                        number += 97; //Add 97 to transform into small
+                    }
+                    else if (letter >= 97 && letter <= 122) //If the letter is small
+                    {
+                        number -= 97; //Subtract by 97
+                        number += key; //Add the key
+                        number %= 26; //Modulo 26
+                        number += 65; //Add 65 to transform into capital
+                    }
+                    caesarText.Append((char)number);
+                }
+            }
+            else
+            {
+                //Go through each character of the plain text
+                for (int i = 0; i < text.Length; i++)
+                {
+                    char letter = text[i];
+                    int number = (int)letter; //Select the ASCII number of the capital letter
+                    //If they are capital
+                    if (letter >= 65 && letter <= 90)
+                    {
+                        number -= 65; //Subtract by 65
+                        number -= key; //Add the key
+                        if (number < 0) //If the number is a negative
+                        {
+                            number += 26; //Convert to positive
+                        }
+                        number %= 26; //Add 26
+                        number += 97; //Add 97 to transform into small
+                    }
+                    else if (letter >= 97 && letter <= 122) //If the letter is small
+                    {
+                        number -= 97; //Subtract by 97
+                        number -= key; //Add the key
+                        if (number < 0) //If the number is a negative
+                        {
+                            number += 26; //Convert to positive
+                        }
+                        number %= 26; //Add 26
+                        number += 65; //Add 65 to transform into capital
+                    }
+                    caesarText.Append((char)number);
+                }
+            }
+            return caesarText.ToString();
+        }
+        //Find key for the encrypted message
+        private void buttonFindKey_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string function = comboBoxFunctions.Text; //Get the selected function
+                string cypherText = textBoxText.Text; //The text in the text box
+                string plainText;
+                string lowerText;
+                List<string> keyList = new List<string>();
+                StringBuilder builder = new StringBuilder();
+                if (function == "Caesar Cypher")
+                {
+                    for (int i = 0; i < 26; i++)
+                    {
+                        plainText = CaesarCypher(cypherText, i, false);
+                        lowerText = plainText.ToLower(); //lower case for checking
+                        if (lowerText.Contains("the") || lowerText.Contains("is") || lowerText.Contains("or")) //Contains any essentiall words
+                        {
+                            keyList.Add(i.ToString());
+                        }
+                    }
+                }
+                builder.Append("Possible keys for " + function + " is : ");
+                foreach (string key in keyList)
+                {
+                    builder.Append(key + ",");
+                }
+                textBoxLog.Text = builder.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Validate inputs");
+            }
+        }
+    }
+}
